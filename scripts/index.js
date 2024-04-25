a4d8dc4a8232ac9425bad63e60977f8ef4e22092
Viktoriya Rohach <v.rohach@admiralmarkets.com>
2024-04-25 16:35:22 +0300

    first_commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const cardTemplate = document.querySelector('#card-template').content;
+const list = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+function createCard(name, link, deleteCardCallback) {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    card.querySelector('.card__title').textContent = name;
+    card.querySelector('.card__image').src = link;
+    const deleteButton = card.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', function () {
+        deleteCardCallback(card);
+    });
+    return card;
+}
 
-// @todo: Функция удаления карточки
+function deleteCard(card) {
+    card.remove();
+}
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function (item) {
+    const card = createCard(item.name, item.link, deleteCard);
+    list.appendChild(card);
+});
